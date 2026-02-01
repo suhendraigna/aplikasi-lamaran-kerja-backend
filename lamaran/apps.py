6242d5275a711f24b681338cfc60bcf2c58a0287
hendra <suhendraigna2@gmail.com>
2026-02-01 11:44:41 +0800

    Inisialisasi project Django dan struktur aplikasi domain

---
+++ lamaran/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LamaranConfig(AppConfig):
+    name = 'lamaran'
---
+++ pelamar/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PelamarConfig(AppConfig):
+    name = 'pelamar'
